518644232f4f5a6d0c29b03d4cae71431ce6747d
gyus13 <gyusss13@naver.com>
2022-05-23 23:27:31 +0900

    google Oauth v1

--- src/users/dto/users.create.dto.ts
+++ src/users/dto/users.create.dto.ts
@@ -1,11 +1,12 @@
 import { User } from '../users.entity';
-import { PickType } from '@nestjs/swagger';
+import {ApiProperty, PickType} from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class UsersCreateDto extends PickType(User, [
   'email',
   'nickname',
 ] as const) {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty({ message: '비밀번호를 작성해 주세요' })
   password: string;
